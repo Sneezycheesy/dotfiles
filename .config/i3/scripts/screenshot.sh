3056cb312db0cba30f765d3a7fdb8f88ddd03e9f
Sneezycheesy <j.niksnl@outlook.com>
2024-06-16 14:09:39 +0200

    Make use of session variables

--- .config/i3/scripts/screenshot.sh
+++ .config/i3/scripts/screenshot.sh
@@ -12,10 +12,10 @@ elif [[ $1 == current ]]; then
 scrot -u -o $image_location
 
 elif [[ $1 == screen0 ]]; then
-scrot -a 0,0,1920,1080 -f -o $image_location
+scrot -a 0,0,$MONITOR_1_WIDTH,$MONITOR_1_HEIGHT -f -o $image_location
 
 elif [[ $1 == screen1 ]]; then
-scrot -a 1920,0,1920,1080 -f -o $image_location
+scrot -a $MONITOR_1_WIDTH,0,$MONITOR_2_WIDTH,$MONITOR_2_HEIGHT -f -o $image_location
 fi
 
 xclip -se c -t image/png -i $image_location
