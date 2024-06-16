a70033181aba753920c898949d3ea051b9809353
Sneezycheesy <j.niksnl@outlook.com>
2024-06-16 14:08:51 +0200

    Allow passing in width to default screen layout script

--- .screenlayouts/default.sh
+++ .screenlayouts/default.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
-WIDTH="$(echo $(( $(xrandr | grep "*" | cut -d" " -f4 | awk -F'x' '{print $1}') / 2 )))"
-HEIGHT="$(echo $(( $(xrandr | grep "*" | cut -d" " -f4 | awk -F'x' '{print $2}'))))"
-xrandr --setmonitor Virtual-1 ${WIDTH}/0x${HEIGHT}/0+0+0 ${HARDWARE_MONITOR_NAME} 
-xrandr --setmonitor Virtual-2 ${WIDTH}/0x${HEIGHT}/0+${WIDTH}+0 ${HARDWARE_MONITOR_NAME}
+WIDTH="$1"
+HEIGHT="$2"
+
+xrandr --setmonitor ${MONITOR_1} ${WIDTH}/0x${HEIGHT}/0+0+0 ${HARDWARE_MONITOR_NAME} 
+xrandr --setmonitor ${MONITOR_2} ${WIDTH}/0x${HEIGHT}/0+${WIDTH}+0 ${HARDWARE_MONITOR_NAME}
+
+MONITOR_1_WIDTH=$WIDTH
+MONITOR_2_WIDTH=$WIDTH
+MONITOR_1_HEIGHT=$HEIGHT
+MONITOR_2_HEIGHT=$HEIGHT
--- .screenlayouts/toggle_monitors.sh
+++ .screenlayouts/toggle_monitors.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/bash
-VIRTUAL_DISPLAY_1="Virtual-1"
-VIRTUAL_DISPLAY_2="Virtual-2"
+MONITOR_1="Virtual-1"
+MONITOR_2="Virtual-2"
+
+FULL_WIDTH=$(xrandr | grep "*" | cut -d" " -f4 | awk -F'x' '{print $1}')
+WIDTH="$(echo $(( $FULL_WIDTH / 2 )))"
+HEIGHT="$(echo $(( $(xrandr | grep "*" | cut -d" " -f4 | awk -F'x' '{print $2}'))))"
+
+echo $WIDTH
 
 function toggle_display {
   if [[ "$1" == "game" || ! -z "$(xrandr --listmonitors | grep Virtual-[12])" ]]; then 
-    xrandr --delmonitor $VIRTUAL_DISPLAY_1
-    xrandr --delmonitor $VIRTUAL_DISPLAY_2
+    xrandr --delmonitor $MONITOR_1
+    xrandr --delmonitor $MONITOR_2
   else 
-    sh $(dirname $0)/default.sh
+    sh $(dirname $0)/default.sh $WIDTH $HEIGHT
   fi
 }
 
 toggle_display $1
+~/.config/polybar/scripts/load_polybar.sh
